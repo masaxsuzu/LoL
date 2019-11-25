38c994b521635387285475812142f523f6a286b9
masaxsuzu <masaxsuzu+github@gmail.com>
2019-11-25 21:30:32 +0900

    ch7 単純なリストの先へ

---
+++ src/ch7/graph.lisp
@@ -0,0 +1,88 @@
+(defparameter *max-label-length* 30)
+(defparameter *wizard-nodes* `((living-room (here is your living room.))
+                        (garden (here is your garden.))
+                        (attic (here is your attic.))))
+(defparameter *wizard-edges* '((living-room (garden west door)
+                                     (attic upstairs ladder))
+                        (garden (living-room east door))
+                        (attic (living-room downstairs ladder))))
+
+(defun dot-name (exp)
+    (substitute-if #\_ (complement #'alphanumericp) (prin1-to-string exp)))
+
+(defun dot-label (exp)
+    (if exp
+        (let ((s (write-to-string exp :pretty nil)))
+            (if (> (length s) *max-label-length*)
+                (concatenate 'string (subseq s 0 (- *max-label-length* 3)) "...")
+                s))
+            ""))
+
+(defun nodes->dot (nodes)
+    (mapc (lambda (node)
+        (fresh-line)
+        (princ (dot-name (car node)))
+        (princ "[label=\"")
+        (princ (dot-label node))
+        (princ "\"];"))
+    nodes))
+
+(defun edges->dot (edges)
+    (mapc (lambda (node)
+        (mapc (lambda (edge)
+            (fresh-line)
+            (princ (dot-name (car node)))
+            (princ "->")
+            (princ (dot-name (car edge)))
+            (princ "[label=\"")
+            (princ (dot-label (cdr edge)))
+            (princ "\"];"))
+        (cdr node)))
+    edges))
+
+(defun uedges->dot (edges)
+    (maplist (lambda (lst)
+        (mapc (lambda (edge)
+            (unless (assoc (car edge) (cdr lst))
+                (fresh-line)
+                (princ (dot-name (caar lst)))
+                (princ "--")
+                (princ (dot-name (car edge)))
+                (princ "[label=\"")
+                (princ (dot-label (cdr edge)))
+                (princ "\"];")))
+            (cdar lst)))
+    edges))
+
+(defun graph->dot (nodes edges)
+    (princ "digraph{")
+    (nodes->dot nodes)
+    (edges->dot edges)
+    (princ "}"))
+
+(defun ugraph->dot (nodes edges)
+    (princ "graph{")
+    (nodes->dot nodes)
+    (uedges->dot edges)
+    (princ "}"))
+
+(defun graph->png (fname nodes edges)
+    (dot->png fname
+            (lambda ()
+                (graph->dot nodes edges))))
+                
+(defun ugraph->png (fname nodes edges)
+    (dot->png fname
+            (lambda ()
+                (ugraph->dot nodes edges))))
+
+(defun dot->png (fname thunk)
+    (with-open-file (*standard-output*
+                    fname
+                    :direction :output
+                    :if-exists :supersede)
+        (funcall thunk))
+    (ext:shell (concatenate 'string "dot -Tpng -O " fname)))
+ 
+(ugraph->png "uwizard.dot" *wizard-nodes* *wizard-edges*)
+(graph->png "wizard.dot" *wizard-nodes* *wizard-edges*)
